358d91b871e91bea063a0bd6f8224098ca80826b
Benjamin Collins <Bcol602@aucklanduni.ac.nz>
2017-09-26 20:58:37 +1300

    Create folder structure

--- scripts/popupScript.js
+++ scripts/popupScript.js
@@ -4,7 +4,7 @@ document.getElementById("page-button").addEventListener('click',openPage)
 
 function openPage() {
     //Create the visualisation tab
-    chrome.tabs.create({url: chrome.extension.getURL('visuals.html')}, function(tab) {
+    chrome.tabs.create({url: chrome.extension.getURL('./pages/visuals.html')}, function(tab) {
         //Callback
     });    
 }
