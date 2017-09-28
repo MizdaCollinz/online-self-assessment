a2d9df734adbf8cb547a0318ea8fef6a1fd5514a
William Lin <wlin888@gmail.com>
2017-09-28 15:29:24 +1300

    Refined time tracking script to be able to work with multiple tabs

--- scripts/track.js
+++ scripts/track.js
@@ -1,17 +1,38 @@
-let startTime, endTime, tabNumber 
+let startTime
 let currentUrl = '';
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     console.log('Updated');
     if (changeInfo.status === "complete" && tab.active) {
-        tabNumber = tabId;
         if (tab.url != 'chrome://newtab/' && tab.url != '') {
             if (currentUrl != tab.url) {
-                endTime = new Date();
-                let timeOnSite = endTime - startTime;
-                console.log('time on site: ' + currentUrl + ' = ' + timeOnSite)
-                currentUrl = tab.url;
-                startTime = new Date();
-                console.log('new time: ' + startTime);
+                chrome.storage.local.get('${currentUrl}', function(result) {
+                    if (result.start == 'undefined') {
+                        startTime = new Date();
+                        let obj = {
+                            'start': startTime,
+                            'end': 0,
+                            'total': 0
+                        };
+
+                        chrome.storage.local.set({'${tab.url}': obj});
+                        currentUrl = tab.url;
+                    } else {
+                        //idk why using startTime instead of result.start works but eh
+                        result.end = new Date() - startTime;
+                        result.total = result.total + result.end;
+                        console.log('time on site: ' + currentUrl + ' = ' + result.end);
+                        startTime = new Date();
+                        console.log('new time: ' + startTime);
+                        let newObj = {
+                            'start': startTime,
+                            'end': 0,
+                            'total': 0
+                        };
+
+                        chrome.storage.local.set({'{$tab.url}': newObj});
+                        currentUrl = tab.url;
+                    }
+                });
             }
         }
     }
@@ -19,10 +40,36 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 
 chrome.tabs.onRemoved.addListener(function (tabId, removeInfo) {
     console.log('Removed');
-    if (tabNumber == tabId) {
-        endTime = new Date();
-        console.log('end time: ' + endTime);
-        let timeOnSite = endTime - startTime;
-        console.log('time on site: ' + timeOnSite);
-    }
+
+    chrome.storage.local.get('${currentUrl}', function(result) {
+        result.end = new Date() - startTime;
+        result.total = result.total + result.end;
+        console.log('time on site: ' + currentUrl + ' = ' + result.end);
+    });
+});
+
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+    console.log('Activated');
+    chrome.tabs.get(activeInfo.tabId, function(tab) {
+        if (tab.url != 'chrome://newtab/' && tab.url != '') {
+            if (currentUrl != tab.url) {
+                chrome.storage.local.get('${currentUrl}', function(result) {
+                    //idk why using startTime instead of result.start works but eh
+                    result.end = new Date() - startTime;
+                    result.total = result.total + result.end;
+                    console.log('time on site: ' + currentUrl + ' = ' + result.end);
+                    startTime = new Date();
+                    console.log('new time: ' + startTime);
+                    let newObj = {
+                        'start': startTime,
+                        'end': 0,
+                        'total': 0
+                    };
+
+                    chrome.storage.local.set({'{$tab.url}': newObj});
+                    currentUrl = tab.url;
+                });
+            }
+        }
+    });
 });
