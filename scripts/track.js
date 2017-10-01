cc8fcee8d9e4b5f1fd6cd6b8dfe80367e0cd89d8
William Lin <wlin888@gmail.com>
2017-10-01 21:06:58 +1300

    Dev timetracking (#1)
    * Created primitive time tracking functionality for a single tab
    
    * Refined time tracking script to be able to work with multiple tabs
    
    * Refactored and changed storage data formats.
    
    It now uses promises to avoid callback hell. Also allows for synchronous
    execution and updates of variables. Changed the storage data format from
    simple json to more detailed objects.
    
    * Updated storage key to use domain name rather than full URL

---
+++ scripts/track.js
@@ -0,0 +1,114 @@
+let startTime, endTime, currentTrack, newTrack, key;
+let currentUrl = '';
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (changeInfo.status === "complete" && tab.active) {
+        if (tab.url != 'chrome://newtab/' && tab.url != '') {
+            let newUrl = new URL(tab.url);
+            let domain = newUrl.hostname;
+            if (currentUrl != domain) {
+                key = domain;
+                currentTrack = new Promise(function(resolve, reject) {
+                    if (currentUrl != '') {
+                        chrome.storage.local.get(`${currentUrl}`, function(result) {
+                            resolve(result[`${currentUrl}`]);
+                        });
+                    }
+                });
+                newTrack = new Promise(function(resolve, reject) {
+                    chrome.storage.local.get(`${key}`, function(result) {
+                        resolve(result[`${key}`]);
+                    });
+                });
+
+                createAndSet(tab);
+            }
+        }
+    }
+});
+chrome.tabs.onRemoved.addListener(function (tabId, removeInfo) {
+    currentTrack = new Promise(function(resolve, reject) {
+        if (currentUrl != '') {
+            chrome.storage.local.get(`${currentUrl}`, function(result) {
+                resolve(result[`${currentUrl}`]);
+            });
+        }
+    });
+
+    updateCurrent();
+});
+
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+    chrome.tabs.get(activeInfo.tabId, function(tab) {
+        if (tab.url != 'chrome://newtab/' && tab.url != '') {
+            let newUrl = new URL(tab.url);
+            let domain = newUrl.hostname;
+            if (currentUrl != domain) {
+                key = domain;
+                currentTrack = new Promise(function(resolve, reject) {
+                    if (currentUrl != '') {
+                        chrome.storage.local.get(`${currentUrl}`, function(result) {
+                            resolve(result[`${currentUrl}`]);
+                        });
+                    }
+                });
+                newTrack = new Promise(function(resolve, reject) {
+                    chrome.storage.local.get(`${key}`, function(result) {
+                        resolve(result[`${key}`]);
+                    });
+                });
+
+                createAndSet(tab);
+            }
+        }
+    });
+});
+
+function createAndSet(tab) {
+    newTrack.then(function(resolvedObj) {
+        let obj;
+        if (typeof resolvedObj === 'undefined') {
+            startTime = new Date();
+            obj = {
+                'visits' : [{
+                    'historyitem' : {},
+                    'time' : {
+                        'start' : `${startTime}`,
+                        'end' : '0'
+                    }
+                }],
+                'tags' : []
+            };
+        } else {
+            startTime = new Date();
+            let newVisit = {
+                'historyitem' : {},
+                'time' : {
+                    'start' : `${startTime}`,
+                    'end' : '0'
+                }
+            };
+            resolvedObj.visits.push(newVisit);
+            obj = resolvedObj;
+        }
+        let dataObj = {};
+        dataObj[key] = obj;
+        chrome.storage.local.set(dataObj);
+
+        updateCurrent();
+
+        return new URL(tab.url).hostname;
+    }).then(function(newUrl) {
+        currentUrl = newUrl;
+    });
+}
+
+function updateCurrent() {
+    currentTrack.then(function(prevObj) {
+        let lastVisit = prevObj.visits[prevObj.visits.length - 1];
+        endTime = new Date();
+        lastVisit.time.end = `${endTime}`;
+        let dataObj = {};
+        dataObj[currentUrl] = prevObj;
+        chrome.storage.local.set(dataObj);
+    });
+}
