181907d19e3189398826f7a93f8fb0330661145d
William Lin <wlin888@gmail.com>
2017-10-03 08:24:53 +1300

    Implemented tagging feature. HTML is still primitive and can be improved

--- scripts/popupScript.js
+++ scripts/popupScript.js
@@ -1,10 +1,40 @@
 
 // Open visualisation page on clicking the page-button
-document.getElementById("page-button").addEventListener('click',openPage)
+//document.getElementById("page-button").addEventListener('click',openPage)
 
 function openPage() {
     //Create the visualisation tab
     chrome.tabs.create({url: chrome.extension.getURL('./pages/visuals.html')}, function(tab) {
         //Callback
-    });    
+    });   
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    let divs = document.querySelectorAll('div');
+    for (let i = 0; i < divs.length; i++) {
+        divs[i].addEventListener('click', function(e) {
+            chrome.tabs.query({
+                'active': true,
+                'currentWindow': true
+            }, function(tabs) {
+                let tabUrl = new URL(tabs[0].url);
+                let domain = tabUrl.hostname;
+            
+                let dataObj = new Promise(function(resolve, reject) {
+                    chrome.storage.local.get(`${domain}`, function(result) {
+                        resolve(result[`${domain}`]);
+                    });
+                }).then(function(resolvedObj) {
+                    if (resolvedObj.tags.indexOf(e.target.id) == -1) {
+                        resolvedObj.tags.push(e.target.id);
+                        let newObj = {};
+                        newObj[domain] = resolvedObj;
+                        chrome.storage.local.set(newObj);
+                    }
+            
+                    window.close();
+                });
+            });
+        });
+    }
+});
