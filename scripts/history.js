b7595ff0c76b30d31755d658301a6bfb30416756
JSCooke <jaydencooke@outlook.com>
2017-10-09 17:29:42 +1300

    Fill out history methods and add comments

--- scripts/history.js
+++ scripts/history.js
@@ -1,38 +1,73 @@
 let currentHistoryItem;
 let currentDomainItem;
+let historyQueryCompleted;
+let domainQueryCompleted;
 
 chrome.history.onVisited.addListener(function(result){
+    
+    // Set up the asynchonous calls to the history APIs
     currentHistoryItem = result;
-    /*  Logging stuff
-    console.log("ID: " + currentHistoryItem.id);
-    console.log("URL: "+ currentHistoryItem.url);    
-    console.log("Title: "+ currentHistoryItem.title);
-    console.log("Last visit time: "+ currentHistoryItem.lastVisitTime);
-    console.log("Visit count: "+ currentHistoryItem.visitCount);
-    console.log("Typed count: "+ currentHistoryItem.typedCount);
-    let newObj = {url:currentHistoryItem.url};
-    chrome.history.getVisits(newObj, function(results){
-        for (i=0; i<results.length; i++){
-            console.log(results[i].visitTime);
-            // The last item in this array is the most recent visit
-        }
+    historyQueryCompleted = new Promise(function(resolve, reject){
+        let urlWrapper = {url:currentHistoryItem.url};
+        chrome.history.getVisits(urlWrapper, function(results){
+            let index = results.length - 1;
+            resolve(results[index].transition);
+        });
     });
-    */
+
+    // Create and set up the domain history object
+    createDomainItem();
+    domainQueryCompleted = new Promise(function(resolve, reject){
+        let urlWrapper = {url:currentDomainItem.entryUrl};
+        chrome.history.getVisits(urlWrapper, function(results){
+            let index = results.length - 1;
+            resolve(results[index].transition);
+        });
+    });
+
 });
 
+// Instantiate the domain item object.
 function createDomainItem(){
-    entryTransition = getTransition(currentHistoryItem);
+    entryTransition = getTransitionFromHistory(currentHistoryItem);
     currentDomainItem = {entryUrl:currentHistoryItem.url, internalClicks:0, transition:entryTransition};
 }
 
-function incrementInternalClicks(historyItem){
-
+// Increase the count of clicks within the current domain
+function incrementInternalClicks(){
+    currentDomainItem.internalClicks++
 }
 
-function getURL(historyItem){
+// Returns the transition for a history item by resolving the promise created on page load.
+function getTransitionFromHistory(historyItem){
+/*
+Alternate callback-based implementation
 
+    let urlWrapper = {url:historyItem.url};  
+    chrome.history.getVisits(urlWrapper, function(results){
+        let index = results.length - 1;
+        return (results[index]).transition;
+    });
+*/
+
+    historyQueryCompleted.then(function (completed) {
+        return completed;
+    });
 }
 
-function getTransition(historyItem){    
-    return "";
+// Returns the the transition for a domain item by resolving the promise created on page load.
+function getTransitionFromDomain(domainItem){ 
+/*
+Alternate callback-based implementation
+
+    let urlWrapper = {url:domainItem.entryUrl};  
+    chrome.history.getVisits(urlWrapper, function(results){
+        let index = results.length - 1;
+        return (results[index]).transition;
+    });
+*/
+
+    domainQueryCompleted.then(function (completed) {
+        return completed;
+    });
 }
