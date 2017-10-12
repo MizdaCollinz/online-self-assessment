9df53bfed5bfef059ed64a5429cc93175e2eb344
JSCooke <jaydencooke@outlook.com>
2017-10-12 16:36:04 +1300

    Switch to callback implementation.
    Callbacks were to be avoided, but in this case, they were easier to get
    working, and used less code.
    Additionally, the history library uses callbacks anyway.

--- scripts/history.js
+++ scripts/history.js
@@ -7,6 +7,9 @@ chrome.history.onVisited.addListener(function(result){
     
     // Set up the asynchonous calls to the history APIs
     currentHistoryItem = result;
+    
+    /*
+    // Promise-based setup
     historyQueryCompleted = new Promise(function(resolve, reject){
         let urlWrapper = {url:currentHistoryItem.url};
         chrome.history.getVisits(urlWrapper, function(results){
@@ -14,9 +17,13 @@ chrome.history.onVisited.addListener(function(result){
             resolve(results[index].transition);
         });
     });
-
+    */
+    
     // Create and set up the domain history object
     createDomainItem();
+
+    /*
+    // Promise-based setup
     domainQueryCompleted = new Promise(function(resolve, reject){
         let urlWrapper = {url:currentDomainItem.entryUrl};
         chrome.history.getVisits(urlWrapper, function(results){
@@ -24,50 +31,54 @@ chrome.history.onVisited.addListener(function(result){
             resolve(results[index].transition);
         });
     });
-
+    */
 });
 
 // Instantiate the domain item object.
 function createDomainItem(){
-    entryTransition = getTransitionFromHistory(currentHistoryItem);
-    currentDomainItem = {entryUrl:currentHistoryItem.url, internalClicks:0, transition:entryTransition};
+    entryTransition = getTransitionFromHistory(currentHistoryItem, function(entryTransition){
+        currentDomainItem = {entryUrl:currentHistoryItem.url, internalClicks:0, transition:entryTransition};
+        console.log(currentDomainItem);
+    });
 }
 
 // Increase the count of clicks within the current domain
 function incrementInternalClicks(){
-    currentDomainItem.internalClicks++
+    currentDomainItem.internalClicks++;
 }
 
-// Returns the transition for a history item by resolving the promise created on page load.
-function getTransitionFromHistory(historyItem){
-/*
-Alternate callback-based implementation
+// Returns the transition for a history item by getting it's most recent visit item.
+function getTransitionFromHistory(historyItem, callback){
 
+// Callback-based implementation
     let urlWrapper = {url:historyItem.url};  
     chrome.history.getVisits(urlWrapper, function(results){
         let index = results.length - 1;
-        return (results[index]).transition;
+        callback((results[index]).transition);
     });
-*/
-
+    
+/*
+// Promise-based implementation
     historyQueryCompleted.then(function (completed) {
         return completed;
     });
+*/
 }
 
-// Returns the the transition for a domain item by resolving the promise created on page load.
-function getTransitionFromDomain(domainItem){ 
-/*
-Alternate callback-based implementation
+// Returns the the transition for a domain item by getting it's most recent visit item.
+function getTransitionFromDomain(domainItem, callback){ 
 
+// Callback-based implementation
     let urlWrapper = {url:domainItem.entryUrl};  
     chrome.history.getVisits(urlWrapper, function(results){
         let index = results.length - 1;
-        return (results[index]).transition;
+        callback((results[index]).transition);
     });
-*/
 
+/*
+// Promise-based implementation
     domainQueryCompleted.then(function (completed) {
         return completed;
     });
+*/
 }
