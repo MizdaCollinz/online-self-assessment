f2a0e05e3d49f22735d70a8e555d4ac6b7fdf4fe
William Lin <wlin888@gmail.com>
2017-10-18 15:35:29 +1300

    Continued attempt to inject feature discovery into web pages

--- scripts/tagDisc.js
+++ scripts/tagDisc.js
@@ -1,11 +1,3 @@
-let iframe = document.createElement("iframe");
-iframe.src = chrome.extension.getURL("./pages/featureDisc.html");
-iframe.setAttribute("style", "border: none; width: 72px; height: 72px; z-index: 999; position: fixed; bottom: 0; right: 0;");
-iframe.setAttribute("id", "tagFrame");
-
-iframe.addEventListener('click', function() {
-    iframe.height('600px');
-    iframe.width('400px');
+$.get(chrome.extension.getURL('/pages/featureDisc.html'), function(data) {
+    $($.parseHTML(data)).appendTo('body');
 });
-
-document.body.appendChild(iframe);
